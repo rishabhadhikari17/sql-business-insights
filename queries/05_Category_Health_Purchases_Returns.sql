83a84f315e45d9b36e0ce552f0bb289da94d5195
Rishabh Adhikari <113086334+rishabhadhikari17@users.noreply.github.com>
2026-06-23 23:46:03 +0530

    Update 05_Category_Health_Purchases_Returns.sql

--- queries/05_Category_Health_Purchases_Returns.sql
+++ queries/05_Category_Health_Purchases_Returns.sql
@@ -1,37 +1,45 @@
 with category_sales as (
-select oi.order_id 
-, oi.variant_id 
-, oi.qty
-, oi.unit_price
-, oi.line_discount
-, oi.line_total
-, c.category_name
+select 
+    c.category_name as category_name
+    , count(distinct oi.order_id) as orders_with_category
+    , sum(oi.qty) as units_sold
+    , sum(oi.unit_price * oi.qty) as revenue
 from ecom.order_items oi
 join ecom.product_variants pv
-on oi.variant_id = pv.variant_id
+    on oi.variant_id = pv.variant_id
 join ecom.products p
-on pv.product_id = p.product_id
+    on pv.product_id = p.product_id
 join ecom.categories c
-on p.category_id = c.category_id
-) 
-, category_returns as(
-select ri.return_id
-, c.category_name
+    on p.category_id = c.category_id
+group by 1
+)
+
+, category_returns as (
+select
+    c.category_name as category_name
+    , count(distinct ri.return_id) as returns
 from ecom.return_items ri
 join ecom.product_variants pv
-on ri.variant_id = pv.variant_id
+    on ri.variant_id = pv.variant_id
 join ecom.products p
-on pv.product_id = p.product_id
-join ecom.categories c 
-on p.category_id = c.category_id
-)
-select cs.category_name
-, count(distinct cs.order_id) as orders_with_category
-, sum(cs.qty) as units_sold
-, sum(cs.qty*cs.unit_price) as revenue
-, count(distinct cr.return_id) as returns
-, 100.00*(count(distinct cr.return_id)::numeric/count(distinct cs.order_id)::numeric) as return_rate
-from category_sales cs
-join category_returns cr
-on cs.category_name= cr.category_name
+    on pv.product_id = p.product_id
+join ecom.categories c
+    on p.category_id = c.category_id
 group by 1
+)
+
+select
+    cs.category_name
+    , cs.orders_with_category
+    , cs.units_sold
+    , cs.revenue
+    , coalesce(cr.returns, 0) as returns
+    , round(
+        100.00 * coalesce(cr.returns, 0)
+        / nullif(cs.orders_with_category, 0)
+        , 2
+    ) as return_rate_pct
+from category_sales cs
+left join category_returns cr
+    on cs.category_name = cr.category_name
+order by return_rate_pct desc;
